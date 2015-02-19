d288b87805423c1f5822c1e32eff8228ca75a320
longzhuan6@gmail.com <longzhuan6@gmail.com@43afd9f6-7c9c-3521-abea-101d3cb365db>
2015-02-19 13:50:36 +0000

    userscript update SwfaccAdkillVideoAntiblack.user.js (Support all of the features from the ADfilter-Blackscreen.user.js and AntiBlackscreenPlus.user.js) New features: Perfect Flash Hardware Acceleration function[Automatically enabled] Added Flash swfvideo Fullscreen to Tab['Shortcuts:alt+enter &or& ctrl+enter enabled'||'Mouse:WebpageLeftmost+Left-click &or& WebpageRightmost+Left-click'] Testing Experience version

--- SwfaccAdkillVideoAntiblack.user.js
+++ SwfaccAdkillVideoAntiblack.user.js
@@ -3,7 +3,7 @@
 // @namespace   Qingtian
 // @author   Qingtian
 // @description   SwfaccAdkillVideoAntiblack
-// @version   2.0.0Nightly+2015.02.16pre
+// @version   2.0.0Nightly+2015.02.18pre
 // @icon   http://code.google.com/p/qtxmd/logo?cct=1391043764
 // @charset   utf-8
 // @downloadURL   http://qtxmd.googlecode.com/svn/SwfaccAdkillVideoAntiblack.user.js
@@ -457,20 +457,19 @@ setTimeout(function () {
   var embeds = document.getElementsByTagName('embed');
   var has_wmode;
   var toggle = function (o) {
-    //console.log(o.id);
     if (o) {
+      var display = o.style.display;
       o.style.display = 'none';
       setTimeout(function () {
-        o.style.display = 'block';
+        o.style.display = '';
       }, 0);
     }
   };
-  //console.log(objects);
   if (objects.length > 0) {
     for (var i = 0; i < objects.length; i++) {
       has_wmode = false;
       for (var ii = 0; ii < objects[i].childNodes.length; ii++) {
-        if (objects[i].childNodes[ii].name.toLowerCase() == 'wmode' || objects[i].childNodes[ii].name.toLowerCase() == 'wMode') {
+        if (objects[i].childNodes[ii].name && objects[i].childNodes[ii].name.toLowerCase() == 'wmode' || objects[i].childNodes[ii].name == 'wMode') {
           has_wmode = true;
           if (force_all) {
             objects[i].childNodes[ii].value = wmodeValue;
@@ -497,18 +496,248 @@ setTimeout(function () {
       if (force_all) {
         embeds[i].setAttribute('wmode', wmodeValue);
         toggle(embeds[i]);
-      }
+      } 
       else if (force_direct_gpu && embeds[i].getAttribute('wmode') == 'direct') {
         embeds[i].setAttribute('wmode', wmodeValue);
         toggle(embeds[i]);
-      }
+      } 
       else if (!embeds[i].getAttribute('wmode')) {
         embeds[i].setAttribute('wmode', wmodeValue);
         toggle(embeds[i]);
       }
     }
   }
-}, 1024);
+}, 100);
+
+(function() {
+
+	var parentArray = new Array(),
+		player = null,
+		fullStatus = false,
+		backStyle = new Array(),
+		childStyle = new Array(),
+		playerStyle, parent, type, iframe;
+
+	function init() {
+		createButton();
+		window.addEventListener("keydown", function(e) {
+			if (e.ctrlKey && e.keyCode == 13 || e.altKey && e.keyCode == 13) {
+				playerControl();
+			}
+		}, false);
+		console.log('Video Player Toothbrush 初始化');
+	}
+
+	function createButton() {
+		var leftButton = document.createElement('span');
+		leftButton.id = 'leftFullStackButton';
+		leftButton.onclick = function() {
+			playerControl();
+		};
+		document.body.appendChild(leftButton);
+		addStyle('#leftFullStackButton{position:fixed;width:1px;height:100%;top:0;left:0;z-index:2147483646;}');
+		var rightButton = document.createElement('span');
+		rightButton.id = 'rightFullStackButton';
+		rightButton.onclick = function() {
+			playerControl();
+		};
+		document.body.appendChild(rightButton);
+		addStyle('#rightFullStackButton{position:fixed;width:1px;height:100%;top:0;right:0;z-index:2147483646;}');
+	}
+
+	function addStyle(css) {
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		var node = document.createTextNode(css);
+		style.appendChild(node);
+		document.head.appendChild(style);
+	}
+
+	function playerControl() {
+		if (!player) {
+			checkPlayer();
+			fullWin();
+		} else {
+			if (!fullStatus) {
+				switch (type) {
+					case 'object':
+						var objectArray = parent.getElementsByTagName('object');
+						checkObject(objectArray);
+						break;
+					case 'embed':
+						var embedArray = parent.getElementsByTagName('embed');
+						checkEmbed(embedArray);
+						break;
+					case 'html5':
+						var html5Array = parent.getElementsByTagName('html5');
+						checkHtml5(html5Array);
+						break;
+					case 'innerIframe':
+					case 'iframe':
+						var iframeArray = parent.getElementsByTagName('iframe');
+						checkIframe(iframeArray);
+						break;
+				}
+				fullWin();
+			} else {
+				smallWin();
+			}
+		}
+	}
+
+	function checkPlayer() {
+		var objectArray = document.getElementsByTagName('object');
+		console.log('object数量' + objectArray.length);
+		checkObject(objectArray);
+		if (!player) {
+			console.log('未找到object播放器');
+			var embedArray = document.getElementsByTagName('embed');
+			console.log('embed数量' + embedArray.length);
+			checkEmbed(embedArray);
+		}
+		if (!player) {
+			console.log('未找到embed播放器');
+			var html5Array = document.getElementsByTagName('video');
+			console.log('html5视频数量' + html5Array.length);
+			checkHtml5(html5Array);
+		}
+		if (!player) {
+			console.log('未找到html5播放器');
+			var iframeArray = document.getElementsByTagName('iframe');
+			console.log('iframe数量' + iframeArray.length);
+			checkIframe(iframeArray);
+		}
+		if (!player) {
+			console.log('未找到iframe引用的播放器');
+			return;
+		}
+		parent = player.parentNode;
+		var full = player;
+		while (full = full.parentNode) {
+			if (full.getAttribute && full.nodeName != 'OBJECT') {
+				full.setAttribute('full_stack', true);
+				parentArray.push(full);
+			}
+			if (full.nodeName == 'HTML') {
+				break;
+			}
+		}
+		if (type == 'innerIframe') {
+			full = iframe;
+			do {
+				if (full.getAttribute) {
+					full.setAttribute('full_stack', true);
+					parentArray.push(full);
+				}
+				if (full.nodeName == 'HTML') {
+					break;
+				}
+			} while (full = full.parentNode);
+		}
+	}
+
+	function checkObject(objectArray) {
+		if (objectArray.length > 0) {
+			for (i = 0; i < objectArray.length; i++) {
+				console.log('object播放器检测' + i);
+				if (objectArray[i].type == 'application/x-shockwave-flash' && objectArray[i].offsetWidth > 256 && objectArray[i].offsetHeight > 128) {
+					player = objectArray[i];
+					type = 'object';
+					console.log('找到object播放器');
+					break;
+				}
+			}
+		}
+	}
+
+	function checkEmbed(embedArray) {
+		if (embedArray.length > 0) {
+			for (i = 0; i < embedArray.length; i++) {
+				console.log('embed播放器检测' + i);
+				if (embedArray[i].type == 'application/x-shockwave-flash' && embedArray[i].offsetWidth > 256 && embedArray[i].offsetHeight > 128) {
+					player = embedArray[i];
+					type = 'embed';
+					console.log('找到embed播放器');
+					break;
+				}
+			}
+		}
+	}
+
+	function checkHtml5(html5Array) {
+		if (html5Array.length > 0) {
+			for (i = 0; i < html5Array.length; i++) {
+				console.log('html5播放器检测' + i);
+				if (html5Array[i].offsetWidth > 256 && html5Array[i].offsetHeight > 128) {
+					player = html5Array[i];
+					type = 'html5';
+					console.log('找到html5播放器');
+					break;
+				}
+			}
+		}
+	}
+
+	function checkIframe(iframeArray) {
+		if (iframeArray.length > 0) {
+			for (var i = 0; i < iframeArray.length; i++) {
+				if (iframeArray[i].offsetWidth > 256 && iframeArray[i].offsetHeight > 128) {
+					try {
+						var objectArray = iframeArray[i].contentWindow.document.getElementsByTagName('object');
+						console.log('iframe' + i + '中object数量' + objectArray.length);
+						checkObject(objectArray);
+						if (!player) {
+							console.log('iframe' + i + '中未找到object播放器');
+							var embedArray = iframeArray[i].contentWindow.document.getElementsByTagName('embed');
+							console.log('iframe' + i + '中embed数量' + embedArray.length);
+							checkEmbed(embedArray);
+						}
+						if (player) {
+							iframe = iframeArray[i];
+							type = 'innerIframe';
+							break;
+						} else {
+							console.log('未找到iframe' + i + '中的播放器');
+						}
+					} catch (e) {
+						player = iframeArray[i];
+						type = 'iframe';
+						console.log('找到可能通过iframe跨域引用的播放器');
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	function fullWin() {
+		if (!fullStatus) {
+			window.addEventListener('resize', fullWin, false);
+			playerStyle = player.style.cssText;
+		}
+		for (var i = 0; i < parentArray.length; i++) {
+			if (!fullStatus) {
+				backStyle[i] = parentArray[i].style.cssText;
+			}
+			parentArray[i].style.cssText = 'width:100% !important;height:100% !important;max-width:100% !important;margin:0px !important;padding:0px !important;top:0px !important;left:0px !important;z-index:2147483645 !important;overflow:hidden !important;position:fixed !important;background:#000 !important;';
+		}
+		player.style.cssText = 'width:calc(100% - 2px) !important;height:100% !important;z-index:2147483645 !important;left:1px !important;position:relative !important;visibility:visible !important;'
+		console.log('网页全屏完成');
+		fullStatus = true;
+	}
+
+	function smallWin() {
+		window.removeEventListener('resize', fullWin, false);
+		for (var i = 0; i < parentArray.length; i++) {
+			parentArray[i].style.cssText = backStyle[i];
+		}
+		player.style.cssText = playerStyle;
+		console.log('恢复完成');
+		fullStatus = false;
+	}
+
+	init();
+})();
 
 (function() {
 	Function.prototype.bind = function() {
